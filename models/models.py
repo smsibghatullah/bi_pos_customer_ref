7ff3c0e482645026d0b96ef4025ce051b174c15e
mubeen1020 <m.mubeen1020@gmail.com>
2024-08-30 15:01:58 +0500

    working

--- models/models.py
+++ models/models.py
@@ -1,8 +1,4 @@
-from odoo import models, fields, api , _
-from datetime import datetime
-from odoo.exceptions import ValidationError, AccessError, UserError
-from odoo.tools import format_datetime
-
+from odoo import api, models
 
 class PosConfig(models.Model):
     _inherit = 'pos.config'
@@ -15,44 +11,3 @@ class PosConfig(models.Model):
             'widget': 'CustomDemoButtons',
         })
         return ui_config
-
-class Attendance(models.Model):
-
-    _inherit = 'hr.attendance'
-
-    def custom_check_in_out(self):
-        employee = self.env['hr.employee'].search([('id', '=', self.id)], limit=1)
-        if employee:
-            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            current_date = datetime.now().strftime('%Y-%m-%d')
-
-            # current_time = datetime.now().strftime('%m/%d/%Y %H:%M:%S') #02/11/2023 15:12:12
-            last_attendance = self.env['hr.attendance'].search([('employee_id', '=', employee.id),('check_in', '<=', current_time)], order='id desc',
-                                                               limit=1)
-            if last_attendance:
-                last_date = last_attendance.check_in.strftime('%Y-%m-%d')
-                if current_date != last_date:
-                    self.env['hr.attendance'].create({
-                        'employee_id': employee.id,
-                        'check_in': current_time,
-                    })
-                    return
-            else:
-                self.env['hr.attendance'].create({
-                    'employee_id': employee.id,
-                    'check_in': current_time,
-                })
-                return
-
-            if last_attendance and last_attendance.check_out == False:
-                last_attendance.write({'check_out': current_time})
-                return
-            else:
-                if last_attendance and last_attendance.check_out and str(last_attendance.check_out) < current_time:
-                    raise ValidationError(
-                        _("Cannot create new attendance record for %(empl_name)s, the employee was already checked in on %(datetime)s") % {
-                            'empl_name': employee.name,
-                            'datetime': format_datetime(self.env, current_time, dt_format=False),
-                        })
-
-
